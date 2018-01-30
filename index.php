ba707aea3b99406c66336819ba170dfbc77e2dee
Hunter Jarrell <hjarrell555@gmail.com>
2018-01-29 21:59:51 -0500

    Created project and connected to database

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+ <head>
+  <title>Secret Voting - SHHHHH</title>
+ </head>
+ <body>
+
+<h1>Secret Votings - SHHHHH ITS SECRET</h1>
+
+ <?php
+ 
+ $servername = "localhost";
+ $username = "root";
+ $password = "root";
+ $dbname = "voting";
+
+ $conn = new mysqli($servername, $username, $password);
+
+ if ($conn->connect_error) {
+     die("Connection failed: ");
+ }
+ 
+ 
+ 
+ 
+ 
+ ?> 
+
+ </body>
+</html>
