13238846191d92bc18aca0af6c723037075fe3c7
Hunter Jarrell <hjarrell555@gmail.com>
2018-05-17 13:43:42 -0400

    Add database test data.
    This allows quick wiping and reloading of the test database.

---
+++ DATABASE_TEST_DATA.sql
@@ -0,0 +1,13 @@
+use voting;
+
+INSERT INTO polls (title) VALUES ('1st Test Poll Y/N/A');
+INSERT INTO polls (title) VALUES ('2nd Test Poll Bob/Joe');
+INSERT INTO polls (title) VALUES ('3rd Test Poll Bob/Joe/No Confidence/Abstain');
+
+INSERT INTO poll_options (poll_id, option_text) VALUES (1,'Yes'), (1, 'No'), (1, 'Abstain');
+INSERT INTO poll_options (poll_id, option_text) VALUES (2,'Bob'), (2, 'Joe');
+INSERT INTO poll_options (poll_id, option_text) VALUES (3,'Bob'), (3, 'Joe'), (3, 'No Confidence'), (3, 'Abstain');
+
+INSERT INTO current_poll VALUES (1,1);
+
+INSERT INTO votes (poll_id, option_id) VALUES (1,1), (1,1), (1,1), (1,1), (1,2), (1,2), (1,3);
