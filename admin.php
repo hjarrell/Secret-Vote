80132974c211a57b7fb712cac021c38d8d4d7b25
Hunter Jarrell <hjarrell555@gmail.com>
2018-05-31 14:00:09 -0400

    Update admin page to view multi-option polls.
    Adds basic support for viewing polls with more than one option.
    Currently only showing a basic table because issue #6 will redesign the admin page.
    
    Part of #11

--- admin.php
+++ admin.php
@@ -32,7 +32,7 @@ session_start(); // Have to start session before html
 
 <h1>Secret Votings - SHHHHH ITS SECRET</h1>
 
-<?php if ($_SESSION["isadmin"] == true){ 
+<?php if ($_SESSION["isadmin"] == true) {
 
     $servername = "127.0.0.1";
     $username = "secretVote";
@@ -44,11 +44,12 @@ session_start(); // Have to start session before html
     if ($conn->connect_error) {
         die("Connection failed: ");
     } else {
-        $checkForCurrentPollSql = "SELECT * from current_poll;";
-        $result = $conn->query($checkForCurrentPollSql);
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_assoc();
-            $name = $row["name"];
+        $checkForCurrentPollSql = "SELECT p.title, p.id from current_poll cp INNER JOIN polls p ON cp.poll_id = p.id;";
+        $currentPollResult = $conn->query($checkForCurrentPollSql);
+        if ($currentPollResult->num_rows > 0) {
+            $currentPoll = $currentPollResult->fetch_assoc();
+            $name = $currentPoll["title"];
+            $pollId = $currentPoll["id"];
 
             echo "<h3>We are currently voting on: <strong>" . htmlspecialchars($name, ENT_QUOTES, 'UTF-8') . "</strong></h3>";
             echo "<br>";
@@ -62,17 +63,17 @@ session_start(); // Have to start session before html
             echo '
                 Start a poll (If you are revoting on someone add a 1(2,3,... and so on for every time) after the name or people wont be able to vote.):
                 <form action="admincommand.php" method="post">
-                    <input type="hidden" name="command" value="startvote"> 
+                    <input type="hidden" name="command" value="startvote">
                     <input type="text" name="name">
                     <input type="submit">
                 </form>
             ';
         }
 
-        // Gets all the names of the people who have votes
-        $nameSql = "SELECT DISTINCT name FROM votes ORDER BY name ASC;";
-        $nameResult = $conn->query($nameSql);
-        if ($nameResult->num_rows > 0) {
+        // Gets every poll in the database
+        $getAllPollsSql = "SELECT id, title FROM polls;";
+        $getAllPollsResult = $conn->query($getAllPollsSql);
+        if ($getAllPollsResult->num_rows > 0) { // Checks to make sure we have at least 1 poll
             // Clear votes button
             echo '
                 <form action="admincommand.php" method="post">
@@ -80,55 +81,63 @@ session_start(); // Have to start session before html
                     <input type="submit" value="CLEAR ALL VOTES!">
                 </form>
             ';
-            // Sets up the table header
-            echo '
-                <table>
-                    <tr>
-                        <th>Name</th>
-                        <th>YES</th>
-                        <th>NO</th>
-                        <th>ABSTAIN</th>
-                        <th>Total</th>
-                    </tr>
-            ';
+            
+            // Loop through each poll
+            while ($pollRow = $getAllPollsResult->fetch_assoc()) {
+                echo "<hr/>";
+                echo "Poll: " . htmlspecialchars($pollRow["title"]);
+                echo "<br/>";
+                // Gets all the poll options and vote counts for the current poll
+                $votesSql = "SELECT DISTINCT COUNT(po.option_text), po.option_text FROM votes v INNER JOIN poll_options po ON v.option_id = po.id WHERE v.poll_id = " . $conn->real_escape_string($pollRow["id"]) . " GROUP BY po.option_text ORDER BY COUNT(po.option_text) DESC;";
+                $votesResult = $conn->query($votesSql);
+                if ($votesResult->num_rows > 0) {
+                    echo "
+                    <table>
+                        <tr>
+                            <th>
+                            Poll Option
+                            </th>
+                            <th>
+                            Votes
+                            </th>
+                        </tr>";
+                    $totalSum = 0;
+                    while ($row = $votesResult->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>";
+                        echo htmlspecialchars($row["option_text"]);
+                        echo "</td>";
+                        echo "<td>";
+                        echo htmlspecialchars($row["COUNT(po.option_text)"]);
+                        echo "</td>";
+                        echo "</tr>";
+                        $totalSum += $row["COUNT(po.option_text)"];
+                    }
+                    echo "<tr>
+                <td><strong>Total</strong></td>
+                <td>";
+                    echo htmlspecialchars($totalSum);
+                    echo "</td></tr>";
 
-            // Loops over each row of names
-            while ($row = $nameResult->fetch_assoc()) {
-                $rowName = $conn->escape_string($row["name"]);
-                // These all count the results of the votes by each type and then the grand total and then fetching the result
-                $yesVotesResult = $conn->query("SELECT COUNT(*) AS total FROM votes WHERE name='". $rowName . "' AND vote='YES';") or die($conn->error);
-                $yesVotes = $yesVotesResult->fetch_assoc()["total"];
-                $noVotesResult = $conn->query("SELECT COUNT(*) AS total FROM votes WHERE name='". $rowName . "' AND vote='NO';");
-                $noVotes = $noVotesResult->fetch_assoc()["total"];
-                $abstainVotesResult = $conn->query("SELECT COUNT(*) AS total FROM votes WHERE name='". $rowName . "' AND vote='ABSTAIN';");
-                $abstainVotes = $abstainVotesResult->fetch_assoc()["total"];
-                $totalVotesResult = $conn->query("SELECT COUNT(*) AS total FROM votes WHERE name='". $rowName . "';");
-                $totalVotes = $totalVotesResult->fetch_assoc()["total"];
-                
-                // Sets up the table row.
-                echo '<tr>';
-                echo '<td>'.$row["name"].'</td>';
-                echo '<td>'.$yesVotes.'</td>';
-                echo '<td>'.$noVotes.'</td>';
-                echo '<td>'.$abstainVotes.'</td>';
-                echo '<td>'.$totalVotes.'</td>';
-                echo '</tr>';
+                    echo "
+            </table>
+            ";
+
+                } else {
+                    echo "There are currently have no votes :(";
+                }
             }
-            // Closes the table started above
-            echo '</table>';
-        } else {
-            echo "You currently have no votes :(";
         }
     }
 
-} else { ?>
+} else {?>
 
     <form action="adminlogin.php" method="post">
         Password: <input type="password" name="password">
         <input type="submit">
     </form>
 
-<?php } ?>
+<?php }?>
 
 </body>
 
